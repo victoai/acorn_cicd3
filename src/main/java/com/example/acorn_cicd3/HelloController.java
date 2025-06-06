2447cfe6030da9feb85cccaee20e26e391f0dcad
victoai <victoai@naver.com>
2025-06-06 22:57:04 +0900

    start

---
+++ src/main/java/com/example/acorn_cicd3/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.acorn_cicd3;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("hi-cicd-docker")
+    public String  hi(){
+
+
+        return  "hi  cicd docker !!!";
+    }
+}
